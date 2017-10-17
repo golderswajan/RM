ae8e686c20688f0e0292dd360e39563af474af87
Swajan Golder <swajan.cse.ku@gmail.com>
2017-10-17 14:17:35 -0700

    db updated

--- resturant.sql
+++ resturant.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 17, 2017 at 09:24 AM
+-- Generation Time: Oct 17, 2017 at 11:16 PM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 7.0.13
 
@@ -76,42 +76,9 @@ CREATE TABLE `foodmenu` (
   `name` varchar(255) NOT NULL,
   `foodimage` varchar(255) NOT NULL,
   `ingredients` text NOT NULL,
-  `price` int(11) NOT NULL
+  `price` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `foodmenu`
---
-
-INSERT INTO `foodmenu` (`id`, `name`, `foodimage`, `ingredients`, `price`) VALUES
-(1, 'Pizza', './images/pizza.jpg', 'Oil.bread', 50),
-(2, 'Burgerss', './images/burger.jpg', 'Chicken,Bread', 120),
-(3, 'Noodles', './images/noodles.jpg', 'noodles,fish,oil,water', 350),
-(5, 'Fried Chicken', './images/fried.jpg', 'chicken', 90);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `foodtype`
---
-
-CREATE TABLE `foodtype` (
-  `id` int(11) NOT NULL,
-  `foodid` int(11) NOT NULL,
-  `typeid` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `migrations`
---
-
-CREATE TABLE `migrations` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -148,33 +115,18 @@ CREATE TABLE `orders` (
 --
 
 CREATE TABLE `type` (
-  `id` int(11) NOT NULL,
-  `time` varchar(255) NOT NULL
+  `type_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `users`
+-- Dumping data for table `type`
 --
 
-CREATE TABLE `users` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'a', 'a@gmail.com', '$2y$10$yiebrY4AWUFtFrjxjCqN3ebi2yMKdYtS0Yc6J./YUXue7aFG1STr2', 'EaCMOXMnFQseKCnP4psEYALqXpDapqx0FNFPz2fuG9Q4YoBQ03gR0RMXE9RG', '2017-10-03 13:01:05', '2017-10-03 13:01:05'),
-(2, 'shuvo', 'md_shuvvoo@yahoo.com', '$2y$10$duFxZDq58ncmJ90j3HoQjOl81TV.pM7EVHlvK0ukXTRAo/rBT1QnG', 'DSzR0s3RfqfCA0dIMrPBMjxDtdyF2BEF6bd1pjJPjsktiAMyr4CMsEkbgNcl', '2017-10-03 23:25:41', '2017-10-03 23:25:41');
+INSERT INTO `type` (`type_name`) VALUES
+('breakfast'),
+('dinner'),
+('lunch'),
+('snacks');
 
 --
 -- Indexes for dumped tables
@@ -203,21 +155,8 @@ ALTER TABLE `feedback`
 -- Indexes for table `foodmenu`
 --
 ALTER TABLE `foodmenu`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `foodtype`
---
-ALTER TABLE `foodtype`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `foodid` (`foodid`),
-  ADD KEY `typeid` (`typeid`);
-
---
--- Indexes for table `migrations`
---
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
+  ADD KEY `type` (`type`);
 
 --
 -- Indexes for table `orderitem`
@@ -238,13 +177,7 @@ ALTER TABLE `orders`
 -- Indexes for table `type`
 --
 ALTER TABLE `type`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`type_name`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -269,12 +202,7 @@ ALTER TABLE `feedback`
 -- AUTO_INCREMENT for table `foodmenu`
 --
 ALTER TABLE `foodmenu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
---
--- AUTO_INCREMENT for table `migrations`
---
-ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `orderitem`
 --
@@ -286,16 +214,6 @@ ALTER TABLE `orderitem`
 ALTER TABLE `orders`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `type`
---
-ALTER TABLE `type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
 -- Constraints for dumped tables
 --
 
@@ -306,11 +224,10 @@ ALTER TABLE `feedback`
   ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`customerid`) REFERENCES `customer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `foodtype`
+-- Constraints for table `foodmenu`
 --
-ALTER TABLE `foodtype`
-  ADD CONSTRAINT `foodtype_ibfk_1` FOREIGN KEY (`typeid`) REFERENCES `type` (`id`),
-  ADD CONSTRAINT `foodtype_ibfk_2` FOREIGN KEY (`foodid`) REFERENCES `foodmenu` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `foodmenu`
+  ADD CONSTRAINT `foodmenu_ibfk_1` FOREIGN KEY (`type`) REFERENCES `type` (`type_name`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `orderitem`
